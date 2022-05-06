35740669d709d30e76fad5fcb2eb01d4d85b4636
pushkarra <pushkarpuru97199@gmail.com>
2022-05-06 22:04:09 +0530

    React lifecycle with the help of useEffect

---
+++ React Lifecycle - L8/Mounting And Unmounting/App.js
@@ -0,0 +1,21 @@
+// Q2 - React lifecycle with the help of useEffect and showing working of Mounting and Unmounting
+
+import React, {useState} from "react";
+import "./App.css";
+import Counterla from "./Counterla";
+
+function App(){
+  const [ showCounters, setShowCounter]=useState(false);
+  return(
+    <div className="App">
+      <h1> Our App</h1>
+      <button onclick={() => setShowCounter(!showCounters)}>
+        {showCounters ? "Hide Counter":"Show Counter"}
+      </button>
+      <Counterla />
+      {showCounters && <Counterla />}
+    </div>
+  );
+}
+
+export default App;
