7f219a8632297cc0a6eb9910d8d3a7050fb6a1c1
Pushkar Rajput <pushkarpuru97199@gmail.com>
2022-05-28 23:57:30 +0530

    Creating a basic react app using router

---
+++ Router Concepts - L10/App.js
@@ -0,0 +1,30 @@
+// Q1- Making a simple single page application
+
+import React from "react";
+import "./App.css";
+import { Route, Link, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import { useNavigate , rest } from "react-router-dom";
+
+function App() {
+  const history = useNavigate();
+  return (
+    <div className="App">
+      <button>
+        <Link to="/about"> About Us </Link>
+      </button>
+      <button onClick={() => history("/contact")}> Contact Us </button>
+      <button onClick={() => history("/home")}> Home Page </button>
+      <Routes>
+        <Route exact path="/about" render={(props) => <About {...props} />} />
+        <Route exact path="/contact" render={(props) => <Contact {...props} />} />
+        <Route exact path="/home" render={(props) => <Home {...props} />} />
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
