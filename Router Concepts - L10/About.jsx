384e3c29367af43460dc247df10c5ec36fc270bc
Pushkar Rajput <pushkarpuru97199@gmail.com>
2022-05-28 23:58:54 +0530

    Using router

---
+++ Router Concepts - L10/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams , rest } from 'react-router-dom';
+
+const About = (props) => {
+
+  console.log(props);
+  return (
+    <div>This is the about page</div>
+
+  );
+};
+
+export default About;
