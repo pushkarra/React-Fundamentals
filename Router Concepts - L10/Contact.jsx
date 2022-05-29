448f2813d0c492bbceb4752123e059543c6702e9
Pushkar Rajput <pushkarpuru97199@gmail.com>
2022-05-29 21:08:35 +0530

    Creating a basic react app using router

--- Router Concepts - L10/Contact.jsx
+++ Router Concepts - L10/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>This is the contact page</div>;
+  return <div>This is the contact page</div>;   // dont know why but it is not displaying over the webpage
 };
 
 export default Contact;
